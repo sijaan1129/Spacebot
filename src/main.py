4044b3dba12f314a99f8ba106d0ec4e7928beb10
sijaan1129 <sijaan53@gmail.com>
2025-07-29 07:36:38 +1200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -21,29 +21,6 @@ from utilities.helpers.utils import get_prefix
 from googletrans import Translator
 translator = Translator()
 
-license = """
-MIT License
-
-Copyright (c) 2021 Dhravya Shah
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE."""
-
 
 install()
 dotenv.load_dotenv()
@@ -125,6 +102,3 @@ for file in track(files, description="Loading all cogs...."):
 
 #* Running the bot
 bot.run(os.getenv("BOT_TOKEN"))
-
-#* for testing
-# bot.run(os.getenv("TEST_BOT_TOKEN"))
